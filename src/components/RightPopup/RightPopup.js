7185d1bf3b00b83dc3f8b714d2012f06b3d5527e
Deva Nandini Bunga <dbunga@AD.MSYSTECHNOLOGIES.COM>
2022-11-30 13:48:08 +0530

    Made some changes

--- src/components/RightPopup/RightPopup.js
+++ src/components/RightPopup/RightPopup.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./RightPopup.scss";
-<<<<<<< HEAD
 import {
   Button,
   Input,
@@ -27,72 +26,29 @@ import Calendar from 'react-calendar';
 import { dummyData, labelBgColorsData ,coverCardsBgColor,coverImages} from "../../Constants/RightPopupData/data";
 import { GrAttachment } from "react-icons/gr";
 import {ContentComponent} from '../ContentComponents/Content'
-=======
 import {Members} from '../Members/members';
 import { Labels } from "../Labels/labels";
 import { Checklist } from "../Checklist/checklist";
 import { Dates } from "../Dates/dates";
 import { Attachment } from "../Attachment/attachment";
 import { Cover } from "../Cover/cover";
->>>>>>> 104efa55b3be1223dae36e0bbb9cdcb5913f6453
 
 
 function RightPopup() {
 
   return (
+    <>
     <div className="right-top-container">
       <h3>Add to card</h3>
-<<<<<<< HEAD
-      {/* {popoverData.map((popoverData)=>(
-        <Popover content={popoverData.content} trigger="click" title={popoverData.title}>
-        </Popover>
-      ))} */}
-      <Popover content={members} trigger="click" title="Members">
-      <Button className="right-container-button">
-        <UserOutlined />
-        Members
-      </Button>
-      </Popover>  
-      <Popover content={labels} trigger='click' title="labels">
-      <Button className="right-container-button">
-        <TagOutlined />
-        Labels
-      </Button>
-      </Popover>
-      <Popover content={checkList} trigger="click" title="Check List">
-      <Button className="right-container-button">
-        <CheckSquareOutlined />
-        Checklist
-      </Button>
-      </Popover>      
-      <Popover content={dates} trigger="click" title="Dates">
-      <Button className="right-container-button">
-        <FieldTimeOutlined />
-        Dates
-      </Button>
-      </Popover>  
-      <Popover content={attachment} trigger="click" title="Attachment">
-      <Button className="right-container-button">
-        <GrAttachment />
-        Attachment
-      </Button> 
-      </Popover>
-      <Popover content={cover} trigger="click" title="Cover">
-      <Button className="right-container-button">
-        <UserOutlined />
-        Cover
-      </Button>
-      </Popover>
-      <ContentComponent/>
-=======
       <Members/>
       <Labels/>   
       <Checklist/>     
       <Dates/>
       <Attachment/>
       <Cover/>
->>>>>>> 104efa55b3be1223dae36e0bbb9cdcb5913f6453
-    </div>
+      <ContentComponent/>
+      </div>
+      </>
   );
 }
 export default RightPopup;
