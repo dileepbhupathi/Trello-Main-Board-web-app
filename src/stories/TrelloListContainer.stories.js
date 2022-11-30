9dbeeb8ee326b017a346aa1f12b6569839305d58
Muliki Varalakshmi <vmuliki@AD.MSYSTECHNOLOGIES.COM>
2022-11-30 16:20:49 +0530

    Vara added stories

---
+++ src/stories/TrelloListContainer.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { ListContainer } from "../components/ListContainer/listContainer";
+
+
+export default{
+    title:'Trello List Container',
+    component:ListContainer
+
+}
+export const MyComponent = () => <ListContainer/>
