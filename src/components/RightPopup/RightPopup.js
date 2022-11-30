9a168bbaa009b74ab15cd0625baf6856042dfc67
Deva Nandini Bunga <dbunga@AD.MSYSTECHNOLOGIES.COM>
2022-11-30 20:53:30 +0530

    Nandini done changes in rightpopup component

--- src/components/RightPopup/RightPopup.js
+++ src/components/RightPopup/RightPopup.js
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
 import "./RightPopup.scss";
-<<<<<<< HEAD
 import { ContentComponent } from "../ContentComponents/Content";
 import { Members } from "../Members/members";
-=======
-import {ContentComponent} from '../ContentComponents/Content'
-import {Members} from '../Members/members';
->>>>>>> 6168d58e3b48595521f5d44fc2ea404a76692a2b
 import { Labels } from "../Labels/labels";
 import { Checklist } from "../Checklist/checklist";
 import { Dates } from "../Dates/dates";
@@ -16,7 +11,6 @@ import { Button, Modal } from "antd";
 import { PicCenterOutlined } from "@ant-design/icons";
 
 function RightPopup() {
-<<<<<<< HEAD
   const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => {
     setIsModalOpen(true);
@@ -24,34 +18,6 @@ function RightPopup() {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
-
-  return (
-    <>
-      <div className="right-top-container">
-        <h3>Add to card</h3>
-        <Members />
-        <Labels />
-        <Checklist />
-        <Dates />
-        <Attachment />
-        <Cover />
-        <Button className="right-container-button" disabled={true}>
-          <PicCenterOutlined />
-          Custom Fields
-        </Button>
-        <p>Add dropdowns, text fields, dates and more to your cards.</p>
-        <Button className="trail-button" onClick={showModal}>
-          Start free trail
-        </Button>
-        <Modal
-=======
-   const [isModalOpen, setIsModalOpen] = useState(false);
-   const showModal = () => {
-      setIsModalOpen(true);
-    };
-    const handleCancel = () => {
-        setIsModalOpen(false);
-      };
 
   return (
     <>
@@ -72,14 +38,12 @@ function RightPopup() {
         Start free trail
       </Button>
       <Modal
->>>>>>> 6168d58e3b48595521f5d44fc2ea404a76692a2b
           mask={false}
           width={500}
           open={isModalOpen}
           onCancel={handleCancel}
           footer={null}
         >
-<<<<<<< HEAD
           <img
             alt="trail"
             src="https://a.trellocdn.com/prgb/dist/images/free-trial/confetti.d5edd99478515693b894.svg"
@@ -105,31 +69,9 @@ function RightPopup() {
           </div>
         </Modal>
         <ContentComponent />
-=======
-        <img alt="trail" src="https://a.trellocdn.com/prgb/dist/images/free-trial/confetti.d5edd99478515693b894.svg"/>
-        <div className="trail-container">
-          <h2>Try Trello Premium free for 14 days</h2>
-          <p>Explore the best of Trello - no credit card required.</p>
-          <ul className="trail-container-list">
-            <li className="trail-container-list-item">Unlimited boards</li>
-            <li className="trail-container-list-item">Advanced checklists</li>
-            <li className="trail-container-list-item">Admin and security features</li>
-            <li className="trail-container-list-item">Unlimited automations</li>
-            <li className="trail-container-list-item">And more!</li>
-          </ul>
-          <button className="trail-container-button">Start free trail</button>
-          <a href="blank" alt="new">Learn more about Trello plan features</a>
-        </div>
-        </Modal>
-      <ContentComponent/>
   
->>>>>>> 6168d58e3b48595521f5d44fc2ea404a76692a2b
       </div>
     </>
   );
 }
-<<<<<<< HEAD
 export default RightPopup;
-=======
-export default RightPopup;
->>>>>>> 6168d58e3b48595521f5d44fc2ea404a76692a2b
