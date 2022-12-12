0ac2f87265fa5654d95804aa8fd15d40503ada5f
Muliki Varalakshmi <vmuliki@AD.MSYSTECHNOLOGIES.COM>
2022-12-12 17:16:47 +0530

    all

--- src/components/ProjBoardContainer/projBoardCotainer.js
+++ src/components/ProjBoardContainer/projBoardCotainer.js
@@ -14,20 +14,40 @@ export const ProjBoardContainer = () => {
   const [boardTitle, setBoardTitle] = useState();
 
   function sumbission(e){
+    // function getContactById(db, id) {
+  //   const txn = db.transaction('lists', 'readonly');
+  //   const store = txn.objectStore('lists');
+
+  //   let query = store.getAll(id);
+  //   query.onsuccess = (event) => {
+  //       if (!request.result) {
+  //           console.log(`The contact with ${id} not found`);
+  //       } else {
+  //           console.log(event.target.result);
+  //           console.log(event.target.result.task);
+  //       }
+  //   };
+  //   query.onerror = () => {
+  //       console.log(request.errorCode);
+  //   }
+
+  //   txn.oncomplete = function () {
+  //       db.close();
+  //   };
+  // };
+  // request.onsuccess = () => {
+  //   const db = request.result;
+  //   getContactById(db, );
+  // };
+  // const items =  db.transaction(lists).objectStore(lists).getAll()
+
       e.preventDefault();
       if (boardTitle !== undefined) {
         dummyListData[uuidv4()] = { name: boardTitle, task: [] };
       }
-  
       setShowAddBoard(false);
   }
 
-
-
-  // function addNewListToBoard() {
-   
-  
-  // }
 
   const onDragEnd = (result, columns, setColumns) => {
     if (!result.destination) return;
@@ -67,6 +87,77 @@ export const ProjBoardContainer = () => {
       });
     }
   };
+
+
+
+  //indexeDB data ............................//
+
+
+
+  const request = indexedDB.open("InitialData", 3);
+  request.onerror = (event) => {
+    console.log(`database error: ${event.target.errorCode}`);
+  };
+  request.onsuccess = () => {
+    console.log("success");
+  };
+
+  //  create the Contacts object store and indexes
+  request.onupgradeneeded = () => {
+    let db = request.result;
+    console.log("db", db); // create the Contacts object store // with auto-increment id
+
+    let store = db.createObjectStore("lists", {
+      keyPath: "index",
+      autoIncrement: true,
+    }); // create an index on the email property
+
+    let index = store.createIndex("Name", "Name", {
+      unique: true,
+    });
+    //    let taskindex = store.createIndex('task', 'task', {
+    //     unique: true
+    // });
+    console.log("index", index);
+    //  console.log("index",taskindex);
+  };
+
+  function insertContact(db, lists) {
+    // create a new transaction
+    const txn = db.transaction(["lists"], "readwrite"); // get the Contacts object store
+    const store = txn.objectStore("lists"); //
+    let query = store.add(lists); // handle success case
+
+    query.onsuccess = function (event) {
+      console.log(event);
+    }; // handle the error case
+
+    query.onerror = function () {
+      // console.log(event.target.errorCode);
+    }; // close the database once the // transaction completes
+
+    txn.oncomplete = function () {
+      db.close();
+    };
+  }
+  request.onsuccess = () => {
+    const db = request.result;
+    insertContact(db, {
+      uniqueId: uuidv4(),
+      Name: "Project Resources",
+      task: [
+        { id: uuidv4(), content: "Weekly Updates" },
+      ],
+    });
+   
+   
+   
+  };
+
+
+
+
+
   return (
     <div className="entire-board-bg">
       <DragDropContext
@@ -88,6 +179,7 @@ export const ProjBoardContainer = () => {
                     // }}
                   >
                     <ul className="list-item">
+                      
                       <ProjBoardCardsContainer
                         eachBoardItem={column}
                         key={columnId}
