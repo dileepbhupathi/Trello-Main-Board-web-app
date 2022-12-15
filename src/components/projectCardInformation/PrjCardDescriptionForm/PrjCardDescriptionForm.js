65d8bd07408708987551cbd91c04209c1cfecd9b
pandala sandeep dharma <spandala@msystechnologies.com>
2022-12-15 11:43:01 +0530

    chnagd stylings of icon

--- src/components/projectCardInformation/PrjCardDescriptionForm/PrjCardDescriptionForm.js
+++ src/components/projectCardInformation/PrjCardDescriptionForm/PrjCardDescriptionForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./PrjCardDescriptionForm.scss";
 import TextArea from "antd/es/input/TextArea";
 import { Form, Space, Button } from "antd";
-import { v4 as uuid } from "uuid";
+// import { v4 as uuid } from "uuid";
 
 const PrjCardDescriptionForm = ({
   setbtndisabled,
@@ -13,7 +13,7 @@ const PrjCardDescriptionForm = ({
   descriptionData,
   CancleButtonLabel,
 }) => {
-  let id = uuid().slice(0, 3)
+  // let id = uuid().slice(0, 3)
   const onValuesChange = (allValues) => {
     if (allValues.data !== undefined && allValues.data !== "") {
       setbtndisabled(false);
@@ -27,46 +27,46 @@ const PrjCardDescriptionForm = ({
     console.log('description submitted : ',e.data)
 
 
-    const request = indexedDB.open("PrjCardInforation", 3);
+    // const request = indexedDB.open("PrjCardInforation", 3);
 
-    const AddDescriptionIntoDb= (db, description) => {
-      const tx = db.transaction(["description"], "readwrite");
-      const store = tx.objectStore("description");
-      let query = store.add(description);
-      query.onsuccess = function (event) {
-        console.log(event);
-      };
+    // const AddDescriptionIntoDb= (db, description) => {
+    //   const tx = db.transaction(["description"], "readwrite");
+    //   const store = tx.objectStore("description");
+    //   let query = store.add(description);
+    //   query.onsuccess = function (event) {
+    //     console.log(event);
+    //   };
 
-      tx.oncomplete = function () {
-        db.close();
-      };
-    };
+    //   tx.oncomplete = function () {
+    //     db.close();
+    //   };
+    // };
 
-    request.onupgradeneeded = () => {
-      let db = request.result;
-      let store = db.createObjectStore("description", {
-        keyPath: "index",
-        autoIncrement: true,
-      });
+    // request.onupgradeneeded = () => {
+    //   let db = request.result;
+    //   let store = db.createObjectStore("description", {
+    //     keyPath: "index",
+    //     autoIncrement: true,
+    //   });
 
-      // let index = store.createIndex("Content", "Content", {
-      //   keyPath: "content",
-      //   unique: true,
-      // });
-      // console.log("index");
-    };
+    //   // let index = store.createIndex("Content", "Content", {
+    //   //   keyPath: "content",
+    //   //   unique: true,
+    //   // });
+    //   // console.log("index");
+    // };
 
-    request.onsuccess = () => {
-      const db = request.result;
-      AddDescriptionIntoDb(db, { id: id, description: e.data });
-      let items = db
-        .transaction(["description"], "readwrite")
-        .objectStore("description")
-        .getAll();
-      items.onsuccess = function (event) {
-        console.log("item success");
-      };
-    };
+    // request.onsuccess = () => {
+    //   const db = request.result;
+    //   AddDescriptionIntoDb(db, { id: id, description: e.data });
+    //   let items = db
+    //     .transaction(["description"], "readwrite")
+    //     .objectStore("description")
+    //     .getAll();
+    //   items.onsuccess = function (event) {
+    //     console.log("item success");
+    //   };
+    // };
     
   };
 
